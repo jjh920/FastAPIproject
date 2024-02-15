a931c606c3037de22bc0904ff7a21b3fb6985ac1
JuHyeon, Cho <jjh920@naver.com>
2024-02-15 11:33:54 +0900

    갤러리6  - 백엔드, 프론트엔드 list

--- app/routes/gallery.py
+++ app/routes/gallery.py
@@ -20,19 +20,21 @@ gallery_router.mount('/static', StaticFiles(directory='views/static'), name='sta
 
 @gallery_router.get('/list/{cpg}', response_class=HTMLResponse)
 def list(req: Request, cpg: int):
-    # stpg = int((cpg - 1) / 10) * 10 + 1     # 페이지네이션 시작값
-    # bdlist, cnt = BoardService.select_board(cpg)
+    stpg = int((cpg - 1) / 10) * 10 + 1     # 페이지네이션 시작값
+    galist, cnt = GalleryService.select_gallery(cpg)
     # allpage = ceil(cnt /25)     # 총 페이지 수
-    return templates.TemplateResponse('gallery/list.html',
-                                      {'request': req, 'gallist': None, 'cpg': cpg, 'stpg': 1, 'allpage': 1, 'baseurl': '/board/list/'})
+    return templates.TemplateResponse(
+        'gallery/list.html',{'request': req, 'galist': galist,
+           'cpg': cpg, 'stpg': 1, 'allpage': 1, 'baseurl': '/gallery/list/'})
+
 
 @gallery_router.get('/list/{ftype}/{fkey}/{cpg}', response_class=HTMLResponse)
 def find(req: Request, ftype: str, fkey: str, cpg: int):
-#     stpg = int((cpg - 1) / 10) * 10 + 1     # 페이지네이션 시작값
-#     bdlist, cnt = BoardService.find_select_board(ftype, '%'+fkey+'%', cpg)
-#     allpage = ceil(cnt /25)     # 총 페이지 수
+    # stpg = int((cpg - 1) / 10) * 10 + 1     # 페이지네이션 시작값
+    # galist, cnt = GalleryService.find_select_gallery(ftype, '%'+fkey+'%', cpg)
+    # allpage = ceil(cnt /25)     # 총 페이지 수
     return templates.TemplateResponse(
-        'gallery/list.html',{'request': req, 'bdlist': None,
+        'gallery/list.html',{'request': req, 'galist': None,
                 'cpg': cpg, 'stpg': 1, 'allpage': 1, 'baseurl': f'/gallery/list/{ftype}/{fkey}/'})
 
 
